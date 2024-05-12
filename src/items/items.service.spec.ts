d92202f79ca4b6724f5db0e363b77383d70da64d
Fabio Rodrigues <fabiordrig@gmail.com>
2024-05-12 13:25:36 -0300

    test: adding initial tests

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,18 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { EntityManager, Repository } from 'typeorm';
+import { Item } from './entities/item.entity';
 import { ItemsService } from './items.service';
 
 describe('ItemsService', () => {
   let service: ItemsService;
+  let itemsRepository: Repository<Item>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ItemsService],
+      providers: [
+        ItemsService,
+        {
+          provide: getRepositoryToken(Item),
+          useValue: {
+            find: jest.fn(),
+          },
+        },
+        {
+          provide: EntityManager,
+          useValue: {
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ItemsService>(ItemsService);
+    itemsRepository = module.get<Repository<Item>>(getRepositoryToken(Item));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create an item', async () => {
+    const item = await service.create({
+      name: 'Item name',
+      public: false,
+      listing: {
+        description: 'Item description',
+      },
+      tags: [
+        {
+          name: 'tag1',
+        },
+      ],
+    });
+
+    expect(item.name).toEqual('Item name');
+  });
+
+  it('should find all items', async () => {
+    await service.findAll();
+
+    expect(itemsRepository.find).toHaveBeenCalled();
   });
 });
